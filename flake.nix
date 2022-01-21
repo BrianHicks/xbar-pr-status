d80744dd1462b1973aaf9a2cf679541e199a0142
Brian Hicks <brian@brianthicks.com>
2022-01-21 11:50:54 -0600

    flake init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    naersk.url = "github:nmattia/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
+        darwinInputs = if pkgs.stdenv.isDarwin then [ pkgs.xcbuild ] else [ ];
+      in rec {
+        # `nix build`
+        packages.xbar-pr-status = naersk-lib.buildPackage {
+          root = ./.;
+          buildInputs = [ pkgs.libiconv pkgs.rustPackages.clippy ]
+            ++ darwinInputs;
+
+          doCheck = true;
+          checkPhase = ''
+            cargo test
+            cargo clippy -- --deny warnings
+          '';
+        };
+        defaultPackage = packages.xbar-pr-status;
+        overlay = final: prev: { xbar-pr-status = packages.xbar-pr-status; };
+
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;
+            [
+              cargo
+              cargo-edit
+              cargo-insta
+              cargo-watch
+              rustPackages.clippy
+              rustc
+              rustfmt
+
+              # for some reason this seems to be required, especially on macOS
+              libiconv
+            ] ++ darwinInputs;
+        };
+      });
+}
