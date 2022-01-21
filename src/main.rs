41313b3aa01bdd63e75a4ab9bc349588c5a3d62f
Brian Hicks <brian@brianthicks.com>
2022-01-21 12:02:40 -0600

    main boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Config {}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = try_main() {
+        println!("{:#?}", err);
+        std::process::exit(1);
+    }
+}
+
+fn try_main() -> Result<()> {
+    let config = Config::parse();
+
+    println!("{:#?}", config);
+
+    Ok(())
 }
